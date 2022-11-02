f2dbd7b9dedba16e2185474f3ed77f534f39a027
冰川 <xx0932399@gmail.com>
2022-11-02 19:07:12 +0800

    修改版本號

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name = "quick-sqlite-database",
-    version = "0.0.2",
+    version = "0.0.3",
     author = "Dada878",
     description = ("A lightweight key-value database basic sqlite"),
     packages=['quick_sqlite'],
