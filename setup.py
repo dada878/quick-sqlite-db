0ade698bea4413ddfed3e5860f7dc3c1de89502e
冰川 <xx0932399@gmail.com>
2022-11-02 19:04:38 +0800

    修改.gitignore

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-setuptools.setup(
-    name = "quick-sqlite-database",
-    version = "0.0.2",
-    author = "Dada878",
-    description = ("A lightweight key-value database basic sqlite"),
-    packages=['quick_sqlite'],
-)
